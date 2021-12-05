aad61d93821bc05dc407ea7f92133ef171fcf130
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-12-04 22:43:52 -0600

    fixed signup page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -57,6 +57,7 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
+    email: req.body.email,
     password: req.body.password,
   })
 
